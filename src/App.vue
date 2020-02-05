3d310ffc176a91c7ceac5f708c992efa96bd8c2b
Adam Frewin <43453625+ajfrewin@users.noreply.github.com>
2020-02-05 16:55:44 -0500

    uploading src

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <Header />
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header';
+export default {
+  name:"app",
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fff;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: rgb(190, 190, 190);
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header class="header">
+        <h1>TodoList</h1>
+        <div>
+            <router-link to="/">Home</router-link>
+            <router-link to='/about'><a>About</a></router-link>
+        </div>
+        <div class="theme-button">Change Theme</div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    .header {
+        background: #333;
+        color: #fff;
+        text-align: center;
+        padding: 10px;
+    }
+    .header a {
+        color: #fff;
+        padding-right: 5px;
+        text-decoration: none;
+    }
+    .theme-button {
+        color: rgb(140,140,140);
+        font-size: 12px;
+    }
+</style>
